4b154164f795444a4f2ba622255c5e7eedac8dd0
Carrie <carr1eg@hotmail.com>
2021-07-13 15:26:41 -0700

    added connection to Mongo

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const morgan = require('morgan');
 const cors = require('cors');
 app.use(express.static('build'));
 app.use(cors()); 
-
+const mongoose = require('mongoose');
 const tokenBody = morgan.token('body', (req) => {
   return JSON.stringify(req.body);
 })
-
 const mgn = morgan(function (tokens, req, res) {
 
   return [
@@ -23,34 +22,25 @@ const mgn = morgan(function (tokens, req, res) {
     tokens.body(req),
   ].join(' ')
 })
-
 app.use(mgn);
 
-const persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
+const url = `mongodb+srv://fullstack:qweasdzxc@cluster0.adag0.mongodb.net/person-app?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema);
+
+
+
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request, response) => {
